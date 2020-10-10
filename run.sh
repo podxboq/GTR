2e9d3048b01bd42184416441f0613473e674ae66
podxboq <podxboq@gmail.com>
2020-10-10 14:02:01 +0200

    Añadimos el soporte para las citas

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
-cd src
-makeglossaries out/main
-pdflatex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=pdf -output-directory=$HOME/Documentos/GTR/out main.tex
+cd $HOME/Documentos/GTR/out 
+#makeglossaries out/main
+makeindex  -s "main.ist" -t "main.glg" -o "main.gls" "main.glo"
+cp ../src/wiki.bib wiki.bib
+bibtex main
+cd ../src
+latex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=dvi -output-directory=$HOME/Documentos/GTR/out main.tex
