1c54fb1acd21f517b6e9d1f694953f2cd964a113
podxboq <podxboq@gmail.com>
2020-10-27 17:10:05 +0100

    Dejamos la notación y la parte inicial del la geometría diferencial

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@ cd $HOME/Documentos/GTR/out
 #makeglossaries out/main
 makeindex  -s "main.ist" -t "main.glg" -o "main.gls" "main.glo"
 cp ../src/wiki.bib wiki.bib
+cp ../src/books.bib books.bib
 bibtex main
 cd ../src
 latex -file-line-error -interaction=nonstopmode -synctex=1 -output-format=dvi -output-directory=$HOME/Documentos/GTR/out main.tex
